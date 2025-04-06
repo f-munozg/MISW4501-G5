06c443f59e25c6714d93fe5734b563d21464ef96
Felipe Muñoz <f.munozg@uniandes.edu.co>
2025-04-05 19:36:51 -0500

    update configs

--- products/app.py
+++ products/app.py
@@ -2,17 +2,17 @@ from flask import Flask
 from flask_restful import Api
 from views.health_check import HealthCheck
 from views.add_product import AddProduct
-
+from models.models import db
 import os
 
 def create_app():
     application = Flask(__name__)
 
-    host = os.environ.get('RDS_HOSTNAME', 'localhost')
-    port = os.environ.get('RDS_PORT', '5432')
-    dbName = os.environ.get('RDS_DB_NAME', 'gcp_db')
-    username = os.environ.get('RDS_USERNAME', 'postgres')
-    password = os.environ.get('RDS_PASSWORD', 'Password123!')
+    host = os.environ.get('DB_HOST', 'localhost')
+    port = os.environ.get('DB_PORT', '5432')
+    dbName = os.environ.get('DB_NAME', 'gcp_db')
+    username = os.environ.get('DB_USERNAME', 'postgres')
+    password = os.environ.get('DB_PASSWORD', 'Password123!')
 
     application.config["SQLALCHEMY_DATABASE_URI"] = f'postgresql://{username}:{password}@{host}:{port}/{dbName}'
     application.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
@@ -38,5 +38,5 @@ def init_db(app):
 
 if __name__ == "__main__":
     application = create_app()
-    port = int(os.environ.get('PORT', 4001))
+    port = int(os.environ.get('APP_PORT', 4001))
     application.run(host='0.0.0.0', port=port)
